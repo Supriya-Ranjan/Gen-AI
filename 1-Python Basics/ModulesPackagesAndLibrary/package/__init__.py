a9061aea2eb70d3ec3a375f1e1b80cdcb9e21e8e
Supriya Ranjan <supriya.ranjan2001@gmail.com>
2026-04-14 16:34:50 +0530

    packages and libary

---
+++ 1-Python Basics/ModulesPackagesAndLibrary/package/__init__.py
@@ -0,0 +1,4 @@
+'''
+This is the package docstring.
+__init__.py is the initialization file for the package. It can be empty, but it can also contain code that runs when the package is imported. This file can also be used to define what is available when the package is imported using the from package import * syntax.
+'''
---
+++ 1-Python Basics/ModulesPackagesAndLibrary/package/subPackages/multi.py
@@ -0,0 +1,6 @@
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
